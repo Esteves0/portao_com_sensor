d09f48a0716b027ca2cb56bb6eda3088adbb42ae
Esteves <guilherme.esteves6@aluno.senai.br>
2026-09-25 10:51:59 -0300

    Adição e conexão com informações do broker e aprimoramento do css

--- app.py
+++ app.py
@@ -9,44 +9,60 @@ MQTT_BROKER = "broker.hivemq.com"
 MQTT_PORT = 1883
 MQTT_TOPIC = "equipe/portao/estado"
 MQTT_TOPIC_DISTANCE = "equipe/portao/distance"
+MQTT_TOPIC_PRESENCE = "equipe/portao/presenca"
 
 app = Flask(__name__)
 app.secret_key = "chave_login_portao"
 
 ultimo_estado = "FECHADO"
+ultima_distancia = "N/A"
+ultima_presenca = "N/A"
+estado_con_broker = "Desconectado"
 
 
 def on_connect(client, userdata, flags, rc, properties=None):
+    global estado_con_broker
     print("[MQTT] Conectado ao Broker!")
+    estado_con_broker ="Conectado!"
     client.subscribe(MQTT_TOPIC)
+    client.subscribe(MQTT_TOPIC_DISTANCE)
+    client.subscribe(MQTT_TOPIC_PRESENCE)
+
     print(f"[MQTT] Inscrito no tópico: {MQTT_TOPIC}")
+    print(f"[MQTT] Inscrito no tópico: {MQTT_TOPIC_DISTANCE}")
 
 
 def on_message(client, userdata, msg):
-    global ultimo_estado
+    global ultimo_estado, ultima_distancia, ultima_presenca
     texto_mensagem = msg.payload.decode()
     print(f"[MQTT] Mensagem recebida no tópico '{msg.topic}': {texto_mensagem}")
 
-    if texto_mensagem == 'ABERTO' and ultimo_estado != 'ABERTO':
-        ultimo_estado = 'ABERTO'
-        print("[MQTT] Portão aberto! Registrando novo acesso no banco de dados...")
+    if msg.topic == MQTT_TOPIC_DISTANCE:
+        ultima_distancia = texto_mensagem
+        print(f"Distancia recebida: {ultima_distancia}")
 
-        try:
-            conn = conexao()
-            cur = conn.cursor()
-            sql = "INSERT INTO acessos(hora_entrada) VALUES (CURRENT_TIMESTAMP)"
-            cur.execute(sql)
-            conn.commit()
-            conn.close()
-        except Exception as e:
-            print(f"[ERRO BANCO] Falha ao inserir registro: {e}")
 
-    elif texto_mensagem == 'FECHADO':
-        ultimo_estado = 'FECHADO'
+    elif msg.topic == MQTT_TOPIC_PRESENCE:
+        ultima_presenca = texto_mensagem
 
-def get_distance(msg):
-    distance = msg.payload.decode()
-    print(f"[MQTT] Distancia recebida: {distance}")
+    elif msg.topic == MQTT_TOPIC:
+        if texto_mensagem == 'ABERTO' and ultimo_estado != 'ABERTO':
+            ultimo_estado = 'ABERTO'
+            print("[MQTT] Portão aberto! Registrando novo acesso no banco de dados...")
+
+            try:
+                conn = conexao()
+                cur = conn.cursor()
+                sql = "INSERT INTO acessos(hora_entrada) VALUES (CURRENT_TIMESTAMP)"
+                cur.execute(sql)
+                conn.commit()
+                conn.close()
+            except Exception as e:
+                print(f"[ERRO BANCO] Falha ao inserir registro: {e}")
+
+        elif ultimo_estado != 'FECHADO' and texto_mensagem == 'FECHADO' :
+            ultimo_estado = 'FECHADO'
+
 
 def iniciar_mqtt():
     try:
@@ -64,6 +80,7 @@ def iniciar_mqtt():
 def home():
     return redirect(url_for('login'))
 
+
 # --- ROTAS ---
 @app.route("/index", methods=['GET'])
 def index():
@@ -78,7 +95,7 @@ def index():
     total_entradas = cur.fetchone()[0]
     conn.close()
 
-    return render_template("index.html", numero_entradas=total_entradas)
+    return render_template("index.html", numero_entradas=total_entradas, distancia=ultima_distancia, presenca = ultima_presenca, estado_broker = estado_con_broker, estado_portao= ultimo_estado)
 
 
 @app.route('/login', methods=['GET', 'POST'])
